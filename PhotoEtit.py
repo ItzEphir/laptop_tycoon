8abe1ca5f83fcd9fab6174bcf9097d5b3be7ea29
Ephir <d.y.dvoryannikov@mail.ru>
2021-06-22 10:45:06 +0300

    Trying to connect our versions

---
+++ PhotoEtit.py
@@ -0,0 +1,12 @@
+from PIL import Image, ImageDraw
+
+
+class Photo:
+    def __init__(self, name):
+        self.image = Image.open(name)
+        self.draw = ImageDraw.Draw(self.image)
+        self.width = self.image.size[0]
+        self.height = self.image.size[1]
+        self.pix = self.image.load()
+
+pass
